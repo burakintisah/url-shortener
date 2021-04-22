109dc8c019bd2007cf0258bec9069f88f6b3b1be
Esra Nur Deniz <esradeniz337@gmail.com>
2021-04-22 22:57:35 +0300

    custom link ui added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,6 +9,8 @@ class Home extends Component {
     state = {
         longUrl: "",
         shortUrl: "",
+        customUrl: "",
+        isCustom: false,
     }
 
     handleSubmit = async event => {
@@ -30,6 +32,27 @@ class Home extends Component {
         }
     };
 
+    handleSubmitCustom = async event => {
+        event.preventDefault();
+        try {
+            const options = {
+                headers: {'Authorization': this.props.auth.user.signInUserSession.idToken.jwtToken,
+                }
+              };
+              var data = { long_url: this.state.longUrl, user_id: this.props.auth.user.attributes.sub, custom_url: this.state.customUrl}
+  //            axios.post('https://83y4xh3vj5.execute-api.eu-central-1.amazonaws.com/test/create', data, options).then(res => {
+//                this.setState({shortUrl: res.data.short_url});   
+     //        });
+        }
+        catch(error) {
+            //console.log(error.message);
+        }
+    };
+
+    handleCustom = event =>{
+        this.setState({isCustom:true});
+    }
+
     onInputChange = event => {
         this.setState({
             [event.target.id]: event.target.value
@@ -43,11 +66,9 @@ class Home extends Component {
             return <Redirect push to={'/login'} />
         }
 
-        //console.log("in home : "+this.props.auth.user.signInUserSession.idToken.jwtToken);
         return (
             <div  >
                 <HomeContainer>
-                    <Form className="home3-form bk"></Form>
                     <Form className="home2-form bk">
                         <Row>
                             <Col sm="3"><h1>URL Shortener</h1></Col>
@@ -59,9 +80,9 @@ class Home extends Component {
                             
                         </Row>
                     </Form>
-                    <Form className="home2-form bk"></Form>
+                    
                     <Form className="home-form bk" onSubmit={this.handleSubmit}>
-                            <h6> Enter a link:</h6>
+                            <h6 className="head"> Enter a link:</h6>
                             <FormGroup className="mt-2 ml-5 mr-5">
                                 <Row>
                                     <Col sm="2"></Col>
@@ -72,14 +93,32 @@ class Home extends Component {
                                         value={this.state.longUrl}
                                         onChange={this.onInputChange} /></Col>
                                     <Col sm="2"><Button className="btn-md mr-5">Shorten</Button></Col>
-                                </Row>
-                                
+                                </Row>                                
                             </FormGroup>
                             
-                            <h6 hidden={this.state.shortUrl === ""}>Shortened URL:</h6> {" " + this.state.shortUrl}
-                            
-                        </Form>
+                    </Form>
+                    <Form className="home-form bk" hidden={!this.state.isCustom} onSubmit={this.handleSubmitCustom}>                            
+                            <h6 className="head2" hidden={!this.state.isCustom}> Enter a custom link:</h6>
+                            <FormGroup className="mt-5 ml-5 mr-5">
+                                <Row>
+                                    <Col sm="2"></Col>
+                                    <Col sm="8"><Input  className=""
+                                        type="text" 
+                                        id="customUrl"
+                                        placeholder="Custom Link"  
+                                        value={this.state.customUrl}
+                                        onChange={this.onInputChange} /></Col>
+                                    <Col sm="2"><Button className="btn-md mr-5">Create Custom Link</Button></Col>
+                                </Row>                                
+                            </FormGroup>                            
+                    </Form>
+                    <Row>
+                        <Col sm="2"></Col>
+                        <Col sm="8"> <h6 hidden={this.state.shortUrl === ""}>Shortened URL:</h6> {" " + this.state.shortUrl} </Col>
+                        <Col sm="2"> <Button className="btn-md mr-5" hidden = {this.state.isCustom} onClick={this.handleCustom}> Create Custom Link</Button> </Col>
+                    </Row>
                 </HomeContainer>
+                
             </div>
         )
     }
@@ -91,18 +130,24 @@ const HomeContainer = styled.div`
     width: 100%;
     max-width: 1800px;
     margin: auto;
-    height: 700px;
+    margin-top:50px;
   }
   .home2-form{
     width: 100%;
     max-width: 1800px;
     margin: auto;
     height: 50px;
+    margin-top: 30px;
   }
-  .home3-form{
-    width: 100%;
-    max-width: 1800px;
-    margin: auto;
-    height: 20px;
-  }
+
+.head {
+    position: absolute;
+    left: 19%;
+    top:13%;
+}
+.head2 {
+    position: absolute;
+    left: 19%;
+    top:25%;
+}
 `;
