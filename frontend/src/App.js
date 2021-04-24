e05ec49e77a626cf409900edd3633f59ee296f85
Esra Nur Deniz <esradeniz337@gmail.com>
2021-04-24 20:48:00 +0300

    add dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,7 @@ class App extends Component {
               <Route exact path="/register" render= {(props) => <Register {...props} auth={authProps}/> }/>
               <Route exact path="/welcome" render= {(props) => <Welcome {...props} auth={authProps}/> }/>
               <Route exact path="/notfound" render= {(props) => <Notfound {...props} auth={authProps}/> }/>
+              <Route exact path="/dashboard" render= {() => (window.location = "https://cloudwatch.amazonaws.com/dashboard.html?dashboard=url_shortener&context=eyJSIjoidXMtZWFzdC0xIiwiRCI6ImN3LWRiLTMwNDYzOTE5MzM1MSIsIlUiOiJ1cy1lYXN0LTFfcnhMWlE2UVhyIiwiQyI6IjduMGx1dDNyZms1anVhOWNnc2RuZWwwMmIwIiwiSSI6InVzLWVhc3QtMTpjNDdiZTk5Yy01YzkxLTQ3NWMtOTUyOC0yMGYzZjBlNWEyNjAiLCJPIjoiYXJuOmF3czppYW06OjMwNDYzOTE5MzM1MTpyb2xlL3NlcnZpY2Utcm9sZS9DbG91ZFdhdGNoRGFzaGJvYXJkLVJlYWRPbmx5QWNjZXNzV2l0aExvZ3MtdXJsX3Nob3J0ZW5lLTlDNEpITk5XIiwiTSI6IlVzclB3U2luZ2xlIn0=")}/>
             </Switch>
           </div>
         </Router>
