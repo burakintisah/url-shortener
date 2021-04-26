51d1b8f49fa4c1616497f4885dc8a10802a27d4f
Şeyma Aybüke Ertekin <55796674+aybukeertekin@users.noreply.github.com>
2021-04-26 19:05:10 +0300

    Delete url-shortener directory

--- url-shortener/mobile-frontend/android/app/src/main/java/com/group2/url_shortener/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.group2.url_shortener;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- url-shortener/mobile-frontend/lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Colors.blue;
-Color primaryDarkColor = Colors.blue.shade900;
-Color primaryLightColor = Colors.blue.shade200;
-
